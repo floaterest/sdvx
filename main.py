fbd283134b92991ece19a134ab8edd5342849cf8
Floaterest <floaterest@outlook.jp>
2020-09-26 20:15:31 -0500

    Refactor

--- main.py
+++ main.py
@@ -11,35 +11,10 @@ toc
         cover
 """
 import os
-import requests
 from html.parser import HTMLParser
 from urllib import request
 
-
-class JSParser:
-    @staticmethod
-    def get_title(line: str):
-        return line[10:]
-
-    @staticmethod
-    def get_artist(line: str):
-        return line[34:-8]
-
-    @staticmethod
-    def get_bpm(line: str):
-        # no regex
-        end = len(line) - 1
-        while not line[end].isdigit():
-            end -= 1
-        begin = (end := end + 1) - 2
-        while (c := line[begin]).isdigit() or c == '-':
-            begin -= 1
-        return line[begin + 1:end]
-
-    @staticmethod
-    def get_yt(line: str):
-        return line[79:90]
-
+import requests
 
 class ToCParser(HTMLParser):
     def __init__(self):
@@ -84,25 +59,43 @@ class SDVX:
             self.url + f'/{song_id[:2]}/jacket/{song_id}n.png',
             os.path.join(path, song_id + '.png'))
 
-    def parse_javascript(self, song_id: str):
+    def get_javascript(self, song_id: str) -> list:
         """
 
         :param song_id: id of the song, e.g. '04265'
         :return:
         """
-        lines = requests.get(self.url + f'/{song_id[:2]}/js/{song_id}sort.js') \
+        return requests.get(self.url + f'/{song_id[:2]}/js/{song_id}sort.js') \
             .content.decode('utf8') \
             .split('\n')
-        # line position is fixed (thank you dev)
-        commands = [
-            [0, JSParser.get_title],
-            [2, JSParser.get_artist],
-            [3, JSParser.get_bpm],
-            [18, JSParser.get_yt]
-        ]
-        for i, cmd in commands:
-            print(cmd(lines[i]))
 
 
-s = SDVX()
-s.parse_javascript('05153')
+class Song:
+    def __init__(self, js: list):
+        self.song_id = js[0][4:9]
+        self.title = js[0][10:]
+        self.composer, self.feat = self.get_artists(js[2])
+        self.bpm = self.get_bpm(js[3])
+        self.ytid = js[18][79:90]
+
+    @staticmethod
+    def get_artists(line: str) -> (str, str):
+        line = line[34:-8]
+        try:
+            i = line.index('feat.')
+            # -1 b/c space
+            return line[:i - 1], line[i:]
+        except ValueError:
+            return line, None
+
+    @staticmethod
+    def get_bpm(line: str):
+        # no regex
+        end = len(line) - 1
+        while not line[end].isdigit():
+            end -= 1
+        begin = (end := end + 1) - 2
+        while (c := line[begin]).isdigit() or c == '-':
+            begin -= 1
+        return line[begin + 1:end]
+
