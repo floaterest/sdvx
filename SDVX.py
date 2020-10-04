0b87b61501c3deff7df49aa2953a3b7d98f700ad
Floaterest <floaterest@outlook.jp>
2020-10-03 19:09:41 -0500

    Refactor SDVX.py

--- SDVX.py
+++ SDVX.py
@@ -1,5 +1,6 @@
 from html.parser import HTMLParser
 from urllib import request
+import html
 
 import requests
 import youtube_dl
@@ -51,24 +52,30 @@ class SDVX:
 
 
 class Song:
-    def __init__(self, js: list, url):
-        self.url = url
-        self.song_id = js[0][4:9]
+    def __init__(self, js: str):
+        lines = [html.unescape(l).strip() for l in js]
+        self.song = lines[0][3:8]
         self.id3 = {
-            'title': js[0][10:],
-            'bpm': self.get_bpm(js[3]),
+            'title': lines[0][9:],
+            'bpm': self.get_bpm(lines[3]),
         }
 
-        self.id3['composer'], self.id3['artist'] = self.get_artists(js[2])
+        self.id3['composer'], self.id3['artist'] = self.get_artists(lines[2])
         # may be empty string
-        self.ytid = js[18][79:90]
+        self.yt = self.get_yt(lines[14:])
 
     @staticmethod
     def get_artists(line: str) -> (str, str):
+        """
+        Parse composer and artist
+        :param line: line of code in javascript
+        :return: composer, feat(can be None)
+        """
+        # composer
         line = line[34:-8]
         try:
             i = line.index('feat.')
-            # -1 b/c space
+            # -1 because space
             return line[:i - 1], line[i:]
         except ValueError:
             return line, None
@@ -79,32 +86,47 @@ class Song:
         end = len(line) - 1
         while not line[end].isdigit():
             end -= 1
+
         begin = (end := end + 1) - 2
         while (c := line[begin]).isdigit() or c == '-':
             begin -= 1
-        return line[begin + 1:end]
+        # if the next char is not a digit, bpm DNE
+        return line[begin + 1:end] if line[begin + 2].isdigit() else 'n/a'
 
-    def download_mp3(self) -> str:
-        if self.ytid:
+    @staticmethod
+    def get_yt(lines: list):
+        """
+        Get YouTube ID, won't raise error if it's empty
+        :param lines:
+        :return:
+        """
+        for l in lines:
+            # the line position of yt url varies
+            if l.startswith('var SD'):
+                return l[79:90]
+        # raise error when html tag not found (can't find the right line in js)
+        raise Exception("can't find youtube url")
+
+    def download_mp3(self, filename: str):
+        if self.yt:
             opt = {
                 'postprocessors': [{
                     'key': 'FFmpegExtractAudio',
                     'preferredcodec': 'mp3',
                     'preferredquality': '192',
                 }],
-                'outtmpl': (filename := f'[{self.song_id}][{self.ytid}]')
+                'outtmpl': filename.replace('.mp3', '')
             }
             with youtube_dl.YoutubeDL(opt) as ydl:
-                ydl.download([f'https://youtu.be/{self.ytid}'])
+                ydl.download([f'https://youtu.be/{self.yt}'])
 
-            return filename + '.mp3'
         else:
             raise Exception('This song is not available on YouTube')
 
-    def download_cover(self):
-        request.urlretrieve(
-            self.url + f'/{self.song_id[:2]}/jacket/{self.song_id}n.png',
-            self.song_id + '.png')
+    # def download_cover(self):
+    #     request.urlretrieve(
+    #         self.url + f'/{self.song_id[:2]}/jacket/{self.song_id}n.png',
+    #         self.song_id + '.png')
 
     def add_tags(self, file):
         # put id3 tags
